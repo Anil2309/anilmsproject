53ff4cd54b92f940851d9966b34b7766b6a40f94
Anil2309 <anilms1234567@gmail.com>
2024-11-13 19:31:34 +0530

    Update2 lines

--- Demo.java
+++ Demo.java
@@ -2,6 +2,7 @@ public class Demo
   {
     public static void main(String [] args
                             int f=100;
+    int j=120020;
                             {
 String name="ANil";
 double dec=2.2;
