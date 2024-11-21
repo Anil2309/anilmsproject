927f3fc3a6da522f75ea877cfd7f977a9dff1c4b
anilms <anilms1234567@gmail.com>
2024-11-21 23:59:31 +0530

    deleted some line

--- Demo.java
+++ Demo.java
@@ -2,10 +2,5 @@ public class Demo
   {
     ()
     public static void main(String [] args
-  int f=100;
-    int j=120020;
-    system.out.println("Hello world.!");
-String Mall="Hii mallikarjun "
-  int k=11;
-long g=gkfkhb;
+
   }
